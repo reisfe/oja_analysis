1ac890adfd1cc2d7291b7e307b04e2b5e81e81b6
Fernando Reis <fernando.reis@ec.europa.eu>
2021-01-20 16:34:57 +0000

    Initial commit

---
+++ scripts/testing_oja.R
@@ -0,0 +1,14 @@
+library(wihoja)
+
+oja <- query_athena("select * from estat_dsl2531b_oja.ft_document_en_v8 limit 10")
+
+oja <- get_oja(idcountry = "LU", lang = "pt")
+
+noja <- get_oja_count(idcountry = "LU", lang = "pt", group_by = "sourcecountry")
+
+noja <- get_oja_count(idcountry = "LU", lang = "pt", group_by = "sourcecountry")
+
+noja <- get_oja_count(group_by = "source")
+
+nojv <- get_ojv_count(idcountry = "LU")
+
